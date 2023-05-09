70c9dba41ce35a53d71839d3878ce2decd80e644
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-09 15:13:54 -0300

    Componente_ SearchForm

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,5 @@
 import { Header } from "../../components/Header";
+import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
 
@@ -9,6 +10,7 @@ export function Transactions() {
       <Summary/>
 
       <TransactionsContainer>
+        <SearchForm/>
         <TransactionTable>
           <tbody>
             <tr>
