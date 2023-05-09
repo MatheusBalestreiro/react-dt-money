518518694297dec98cc66c0edb539cde2df04d44
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-09 14:03:52 -0300

    Tabela de transacão

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionTable, TransactionsContainer } from "./styles";
 
 export function Transactions() {
   return(
     <div>
       <Header/>
       <Summary/>
+
+      <TransactionsContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 1.250,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/05/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Fatura do cartão</td>
+              <td>
+              <PriceHighlight variant="outcome">
+                  - R$ 250,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/05/2023</td>
+            </tr>    
+          </tbody>
+        </TransactionTable>
+      </TransactionsContainer>
     </div>
   )
 }
